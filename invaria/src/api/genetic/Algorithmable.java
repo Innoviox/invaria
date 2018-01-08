f20b0225786edb782cae8c622de058dddc5b4c98
FriendlySinusoid <s.f.skarpiphi@gmail.com>
2018-01-08 11:49:42 -0500

    Add basic classes

--- invaria/src/api/genetic/Algorithmable.java
+++ invaria/src/api/genetic/Algorithmable.java
@@ -1,5 +1,17 @@
 package api.genetic;
 
-public interface Algorithmable {
+import java.util.List;
 
+public interface Algorithmable {
+	
+	public double getFitness();
+	
+	public void setFitness(double d);
+	
+	public void findFitness(Object target);
+	
+	public Algorithmable cross(Algorithmable a);
+	
+	public void mutate();
+	
 }
--- invaria/src/api/genetic/Algorizer.java
+++ invaria/src/api/genetic/Algorizer.java
@@ -1,5 +1,92 @@
 package api.genetic;
 
-public class Algorizer {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Algorizer<T extends Algorithmable, E> {
+
+	protected List<T> specimens = new ArrayList<>();
+	protected int sampleSize;
+	protected double threshhold;
+	protected E target;
+
+	public abstract T genElement();
+
+	public void initializeSet() {
+		boolean safe = false;
+		while (!safe) {
+			while (specimens.size() < sampleSize) {
+				specimens.add(genElement());
+			}
+			calculateFitness();
+			for (T specimen : specimens) {
+				if(specimen.getFitness() != 0){
+					safe = true;
+				}
+			}
+		}
+
+	}
+
+	public void select() {
+		calculateFitness();
+		Iterator<T> i = specimens.iterator();
+		while (i.hasNext()) {
+			T temp = i.next();
+			if (temp.getFitness() < threshhold)
+				i.remove();
+		}
+	}
+
+	public void crossover() {
+		while (specimens.size() < sampleSize) {
+			specimens.add((T) specimens.get(new Random().nextInt(specimens.size())).cross(specimens.get(new Random().nextInt(specimens.size()))));
+		}
+	}
+
+	public void mutate(){
+		for(T specimen : specimens){
+			specimen.mutate();
+		}
+	}
+	
+	
+	private void calculateFitness() {
+		for (T specimen : specimens) {
+			specimen.findFitness(target);
+		}
+		normalize(specimens);
+		double sum = 0;
+		for (T specimen : specimens) {
+			double tempFitness = specimen.getFitness();
+			specimen.setFitness(specimen.getFitness() + sum);
+			sum += tempFitness;
+		}
+	}
+
+	private void normalize(List<T> spec) {
+		double sum = 0;
+
+		// may need to swap a and b
+		spec.sort((b, a) -> Double.compare(b.getFitness(), a.getFitness()));
+
+		for (T specimen : specimens) {
+			sum += specimen.getFitness();
+		}
+
+		if(sum == 0){
+			return;
+		}
+		
+		for (T specimen : specimens) {
+			specimen.setFitness(specimen.getFitness() / sum);
+		}
+
+	}
+	public List<T> output(){
+		return new ArrayList<T>(specimens);
+	}
 
 }
